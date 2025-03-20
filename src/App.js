40b40400cd1724c90674c28c601ee658acea7d3e
jeferson-gallo <jdgap9801@gmail.com>
2025-03-20 10:57:34 -0500

    Tutorial finished with local mock json server

--- src/App.js
+++ src/App.js
@@ -57,25 +57,79 @@ function App() {
     return data
   };
 
-  // Delete a task //
-  const deleteTask = (id) => {
+  // Fetch task //
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    console.log(data);
+    return data
+  };
+
+  // // Delete a task //
+  // const deleteTask = (id) => {
+  //   console.log("Delete: ", id)
+  //   setTasks(tasks.filter((task) => task.id !== id))
+  // }
+  // Delete a task from json server //
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "DELETE",
+    });
+    
     console.log("Delete: ", id)
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  // // Toggle reminder //
+  // const toggleReminder = (id) => {
+  //   console.log("Reminder:", id)
+  //   setTasks(tasks.map((task) =>
+  //     task.id === id ? { ...task, reminder: !task.reminder } : task))
+  // }
+
   // Toggle reminder //
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updTask = {...taskToToggle,
+      reminder: !taskToToggle.reminder
+    }
+    
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(updTask)
+    });
+
+    const data = await res.json()
+    console.log(data.reminder)
+    
     console.log("Reminder:", id)
     setTasks(tasks.map((task) =>
-      task.id === id ? { ...task, reminder: !task.reminder } : task))
+      task.id === id ? { ...task, reminder: data.reminder } : task))
   }
 
-  // Add task //
-  const addTask = (task) => {
-    console.log(task)
-    const id = Math.floor(Math.random() * 1000) + 1
-    const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
+  // // Add task //
+  // const addTask = (task) => {
+  //   console.log(task)
+  //   const id = Math.floor(Math.random() * 1000) + 1
+  //   const newTask = { id, ...task }
+  //   setTasks([...tasks, newTask])
+  // }
+
+  // Add task for json server //
+  const addTask = async (task) => {
+    const res = await fetch("http://localhost:5000/tasks",{
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+    setTasks([...tasks, data])
   }
 
   return (
